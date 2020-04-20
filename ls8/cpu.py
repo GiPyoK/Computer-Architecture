a6bb9c622414bc76aef5cb2be215eed7beade9a5
Gi Pyo Kim <gipyok@gmail.com>
2020-04-20 15:21:07 -0700

    Added ram_read() function, ram_write() funtion; Added instructions; Implemented LDI, PRN, and HLT cases

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,12 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.pc = 0 # Program Counter
+        self.fl = 0 # flag
+        self.ram = [0] * 256 # bytes of memory
+        self.reg = [0] * 8 # 8 general purpose registers
+        self.reg[7] = 0xF4 # default value
+
 
     def load(self):
         """Load a program into memory."""
@@ -62,4 +67,71 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+
+        # Instructions
+        ## ALU ops
+        ADD  = 0b10100000
+        SUB  = 0b10100001
+        MUL  = 0b10100010
+        DIV  = 0b10100011
+        MOD  = 0b10100100
+        INC  = 0b01100101
+        DEC  = 0b01100110
+        CMP  = 0b10100111
+        AND  = 0b10101000
+        NOT  = 0b01101001
+        OR   = 0b10101010
+        XOR  = 0b10101011
+        SHL  = 0b10101100
+        SHR  = 0b10101101
+        ## PC mutators
+        CALL = 0b01010000
+        RET  = 0b00010001     
+        INT  = 0b01010010
+        IRET = 0b00010011
+        JMP  = 0b01010100
+        JEQ  = 0b01010101
+        JNE  = 0b01010110
+        JGT  = 0b01010111
+        JLT  = 0b01011000
+        JLE  = 0b01011001
+        JGE  = 0b01011010
+        ## Other
+        NOP  = 0b00000000
+        HLT  = 0b00000001 
+        LDI  = 0b10000010
+        LD   = 0b10000011
+        ST   = 0b10000100
+        PUSH = 0b01000101 
+        POP  = 0b01000110 
+        PRN  = 0b01000111 
+        PRA  = 0b01001000
+
+        running = True
+        while running:
+            # store instruction in ram to 'Instruction Register'
+            ir = self.ram_read(self.pc)
+
+            if ir == LDI:
+                reg_num = self.ram_read(self.pc+1)
+                value = self.ram_read(self.pc+2)
+                self.reg[reg_num] = value
+                self.pc += 3
+
+            # Print commend
+            elif ir == PRN:
+                reg_num = self.ram_read(self.pc+1)
+                print(self.reg[reg_num])
+                self.pc += 2
+
+            elif ir == HLT:
+                running = False
+
+
+    def ram_read(self, address):
+        """accept the address to read and return the value stored there"""
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        """accept a value to write, and the address to write it to"""
+        self.ram[address] = value
