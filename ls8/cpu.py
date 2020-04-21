b7436c7b9b58213b5d5a7cbc220d6f24aeffbab1
Gi Pyo Kim <gipyok@gmail.com>
2020-04-21 14:58:07 -0700

    Simplified run()

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -12,6 +12,51 @@ class CPU:
         self.ram = [0] * 256 # bytes of memory
         self.reg = [0] * 8 # 8 general purpose registers
         self.reg[7] = 0xF4 # default value
+        self.ops = {}
+        self.set_ops()
+
+    def set_ops(self):
+        # Instructions
+        ## ALU ops
+        ADD  = 0b10100000
+        SUB  = 0b10100001
+        MUL  = 0b10100010
+        DIV  = 0b10100011
+        MOD  = 0b10100100
+        INC  = 0b01100101
+        DEC  = 0b01100110
+        CMP  = 0b10100111
+        AND  = 0b10101000
+        NOT  = 0b01101001
+        OR   = 0b10101010
+        XOR  = 0b10101011
+        SHL  = 0b10101100
+        SHR  = 0b10101101
+        ## PC mutators
+        CALL = 0b01010000
+        RET  = 0b00010001     
+        INT  = 0b01010010
+        IRET = 0b00010011
+        JMP  = 0b01010100
+        JEQ  = 0b01010101
+        JNE  = 0b01010110
+        JGT  = 0b01010111
+        JLT  = 0b01011000
+        JLE  = 0b01011001
+        JGE  = 0b01011010
+        ## Other
+        NOP  = 0b00000000 
+        LDI  = 0b10000010
+        LD   = 0b10000011
+        ST   = 0b10000100
+        PUSH = 0b01000101 
+        POP  = 0b01000110 
+        PRN  = 0b01000111 
+        PRA  = 0b01001000
+
+        self.ops[LDI] = self.ldi
+        self.ops[PRN] = self.prn
+        self.ops[MUL] = self.mul
 
 
     def load(self, file_name):
@@ -37,6 +82,20 @@ class CPU:
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
+
+    def ldi(self):
+        reg_num = self.ram_read(self.pc + 1)
+        value = self.ram_read(self.pc + 2)
+        self.reg[reg_num] = value
+
+    def prn(self):
+        reg_num = self.ram_read(self.pc + 1)
+        print(self.reg[reg_num])
+
+    def mul(self):
+        reg_a = self.ram_read(self.pc + 1)
+        reg_b = self.ram_read(self.pc + 2)
+        self.reg[reg_a] *= self.reg[reg_b]
 
     def trace(self):
         """
@@ -68,69 +127,17 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-
-        # Instructions
-        ## ALU ops
-        ADD  = 0b10100000
-        SUB  = 0b10100001
-        MUL  = 0b10100010
-        DIV  = 0b10100011
-        MOD  = 0b10100100
-        INC  = 0b01100101
-        DEC  = 0b01100110
-        CMP  = 0b10100111
-        AND  = 0b10101000
-        NOT  = 0b01101001
-        OR   = 0b10101010
-        XOR  = 0b10101011
-        SHL  = 0b10101100
-        SHR  = 0b10101101
-        ## PC mutators
-        CALL = 0b01010000
-        RET  = 0b00010001     
-        INT  = 0b01010010
-        IRET = 0b00010011
-        JMP  = 0b01010100
-        JEQ  = 0b01010101
-        JNE  = 0b01010110
-        JGT  = 0b01010111
-        JLT  = 0b01011000
-        JLE  = 0b01011001
-        JGE  = 0b01011010
-        ## Other
-        NOP  = 0b00000000
-        HLT  = 0b00000001 
-        LDI  = 0b10000010
-        LD   = 0b10000011
-        ST   = 0b10000100
-        PUSH = 0b01000101 
-        POP  = 0b01000110 
-        PRN  = 0b01000111 
-        PRA  = 0b01001000
-
+        HLT  = 0b00000001
         running = True
         while running:
             # store instruction in ram to 'Instruction Register'
             ir = self.ram_read(self.pc)
             inst_len = (ir >> 6) + 0b1
 
-            if ir == LDI:
-                reg_num = self.ram_read(self.pc+1)
-                value = self.ram_read(self.pc+2)
-                self.reg[reg_num] = value
-
-            elif ir == PRN: # Print
-                reg_num = self.ram_read(self.pc+1)
-                print(self.reg[reg_num])
-
-            elif ir == MUL: # Multiply
-                reg_a = self.ram_read(self.pc+1)
-                reg_b = self.ram_read(self.pc+2)
-                self.reg[reg_a] *= self.reg[reg_b]
-
-            elif ir == HLT: # Halt
+            if ir in self.ops:
+                self.ops[ir]()
+            elif ir == HLT:
                 running = False
-            
             else:
                 print("Invalid Instruction. Exiting LS8.")
                 break
